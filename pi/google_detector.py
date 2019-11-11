a586b97eb59d338c5bce4e5079d9cc4c9ddc2ce6
Oliver Choy <oliver.choy@slalom.com>
2019-11-10 17:26:22 -0800

    final version before hackathon

--- pi/google_detector.py
+++ pi/google_detector.py
@@ -3,9 +3,12 @@ import scipy.io.wavfile as wav
 import time
 import calendar
 import wave
+import seeed_dht
+import requests,json
 from audio.processor import WavProcessor, format_predictions
 from mic_array import MicArray
 from pixels import pixels
+from collections import deque, Counter
 
 RATE = 16000
 CHANNELS = 4
@@ -14,6 +17,8 @@ CHUNK_LENGTH = 78
 
 with WavProcessor() as proc:
     chunks = []
+    chainsawQ = deque([0,0,0,0,0,0,0,0])
+    createCase = 0
 
     try:
         with MicArray(RATE, CHANNELS, CHUNK_SIZE)  as mic:
@@ -23,9 +28,6 @@ with WavProcessor() as proc:
                 chunks.append(chunk)
                 if len(chunks) == CHUNK_LENGTH:
                     frames = np.concatenate(chunks)
-
-                    direction = mic.get_direction(frames)
-                    pixels.wakeup(direction)
 
                     wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
                     wf.setnchannels(CHANNELS)
@@ -40,9 +42,47 @@ with WavProcessor() as proc:
 
                     predictions = proc.get_predictions(rate, sig)
                     prettyPrediction = format_predictions(predictions)
+                    
+                    lowerP = prettyPrediction.lower()
+                    recognizable = "vehicle" in lowerP or "chainsaw" in lowerP or "car" in lowerP or "lawn" in lowerP or "animal" in lowerP or "manmade" in lowerP
+                    containsChainsaw = "chainsaw" in lowerP
 
-                    if (prettyPrediction):
-                        print('Timestamp: {} | Detection: {} | Position: {}'.format(str(ts), prettyPrediction, int(direction)))
+                    headers = {'Content-Type': 'application/json'}
+                    endpoint = 'http://192.168.1.1:3000'
+
+                    if (prettyPrediction and recognizable):
+                        direction = mic.get_direction(frames)
+                        pixels.wakeup(direction)
+                        sensor = seeed_dht.DHT("22", 12)
+                        humi, temp = sensor.read()
+
+                        chainsawQ.popleft()
+                        if (containsChainsaw):
+                            chainsawQ.append(1)
+                        else:
+                            chainsawQ.append(0)
+
+                        if not humi is None:
+                            print('Timestamp: {0} | Detection: {1} | Position: {2} | Humidity {3:.1f}% | Temperature {4:.1f}'.format(str(ts), prettyPrediction, int(direction), humi, temp))
+                        else:
+                            print('Timestamp: {} | Detection: {} | Position: {}'.format(str(ts), prettyPrediction, int(direction)))
+
+                        chainsawSum = sum(chainsawQ)
+                        print('queue:{}, sum:{}'.format(chainsawQ, chainsawSum))
+
+                        if (chainsawSum >= 3):
+                            print('Chainsaw occurred multiple times. File Case!')
+                            createCase = 1
+                        else:
+                            createCase = 0
+
+                        body = {'Predicted_Sound__c': prettyPrediction, 'Device_Number__c': 'ABC123', 'Position__c': int(direction), 'Temperature__c': round(temp, 2), 'Humidity__c': round(humi, 2), 'Create_Case__c': createCase}
+                        #requests.post(endpoint,data=json.dumps(body),headers=headers)
+
+                        if (chainsawSum >= 3):
+                            chainsawQ = deque([0,0,0,0,0,0,0,0])
+                    else:
+                        print('Timestamp: {} | Unrecognizable noise'.format(str(ts)))
 
                     chunks = []
 
